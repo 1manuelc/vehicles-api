a48f5a526d1710307a8691668a5fa481af2600d0
1manuelc <manuelwn21@gmail.com>
2025-06-11 12:19:41 -0300

    feat: adds country and foundedAt to query filtering

--- src/controllers/brand/brandController.ts
+++ src/controllers/brand/brandController.ts
@@ -13,7 +13,7 @@ export class BrandController {
 		try {
 			let brands = await this.service.getBrands();
 
-			const { name } = req.query;
+			const { name, country, foundedAt } = req.query;
 
 			if (name) {
 				brands = brands.filter((brand) =>
@@ -21,12 +21,22 @@ export class BrandController {
 				);
 			}
 
+			if (country) {
+				brands = brands.filter((brand) =>
+					brand.country.toLowerCase().includes(String(country).toLowerCase())
+				);
+			}
+
+			if (foundedAt) {
+				brands = brands.filter(
+					(brand) => brand.foundedAt === Number(foundedAt)
+				);
+			}
+
 			return res.status(200).json({
 				success: true,
 				// formata mensagem com plural ou singular para brands
-				message: `${brands.length} brand${brands.length > 1 ? 's' : ''}${
-					name ? ` with name containing '${name}'` : ''
-				} found`,
+				message: `${brands.length} brand${brands.length > 1 ? 's' : ''} found`,
 				// formata saída para query param de filtragem aparecer em message
 				data: brands.sort((a, b) => a.id - b.id),
 				// ordena por id crescente,
@@ -60,9 +70,7 @@ export class BrandController {
 				throw new ApiError(404, 'invalid brand information provided');
 			}
 
-			const { name } = req.body;
-
-			const brand = await this.service.createBrand({ name });
+			const brand = await this.service.createBrand({ ...req.body });
 			if (!brand) {
 				throw new ApiError(500, 'there was an error creating the brand');
 			}
@@ -78,9 +86,8 @@ export class BrandController {
 
 	async updateBrandById(req: Request, res: Response, next: NextFunction) {
 		try {
-			const { name } = req.body;
 			const updated = await this.service.patchBrandById(Number(req.params.id), {
-				name,
+				...req.body,
 			});
 
 			if (!updated) {
